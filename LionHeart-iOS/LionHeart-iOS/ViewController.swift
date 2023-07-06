13a6a4cc15f2e154d704d4fb29c40ee6730b27ad
Kim Minjae <ffalswo2@naver.com>
2023-07-06 18:00:13 +0900

    :seedling: 폴더링 구조 세팅 (#1)

--- LionHeart-iOS/LionHeart-iOS/ViewController.swift
+++ LionHeart-iOS/LionHeart-iOS/ViewController.swift
@@ -5,9 +5,13 @@
 //  Created by 김민재 on 2023/07/05.
 //
 
+
+// MARK: - 지워도 되는 파일
 import UIKit
 
 class ViewController: UIViewController {
+
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
