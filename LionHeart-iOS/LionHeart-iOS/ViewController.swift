066d5da6d68fc6df363707bd40e33edda7af31fd
Kim Minjae <ffalswo2@naver.com>
2023-07-06 17:18:21 +0900

    :tada: Project setting (#1)
    provisioning file setting and xcconfig

---
+++ LionHeart-iOS/LionHeart-iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LionHeart-iOS
+//
+//  Created by 김민재 on 2023/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .blue
+    }
+
+
+}
+
