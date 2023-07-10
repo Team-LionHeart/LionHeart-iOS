8e3876d4158d2ab9a8c9ddc0e6bf40986561e333
Kim Minjae <ffalswo2@naver.com>
2023-07-10 10:54:10 +0900

    :recycle: NavigationBarLayoutManager 추가 (#7)
    addSubView와 레이아웃 설정 간소화

--- LionHeart-iOS/LionHeart-iOS/Global/Extensions/UIApplication+.swift
+++ LionHeart-iOS/LionHeart-iOS/Global/Extensions/UIApplication+.swift
@@ -5,4 +5,22 @@
 //  Created by 김민재 on 2023/07/10.
 //
 
-import Foundation
+import UIKit
+
+
+extension UIApplication {
+    static func navigationTopViewController(base: UIViewController? = UIApplication.keyWindow?.rootViewController) -> UIViewController? {
+        if let nav = base as? UINavigationController {
+            return navigationTopViewController(base: nav.visibleViewController)
+        }
+        return base
+    }
+}
+
+extension UIApplication {
+    static var keyWindow: UIWindow? {
+        let scenes = UIApplication.shared.connectedScenes
+        let windowScene = scenes.first as? UIWindowScene
+        return windowScene?.windows.first
+    }
+}
--- LionHeart-iOS/LionHeart-iOS/Global/Utils/NavigationBarLayoutManager.swift
+++ LionHeart-iOS/LionHeart-iOS/Global/Utils/NavigationBarLayoutManager.swift
@@ -5,4 +5,18 @@
 //  Created by 김민재 on 2023/07/10.
 //
 
-import Foundation
+import UIKit
+
+final class NavigationBarLayoutManager {
+    static func add(_ naviView: LHNavigationBarView) {
+
+        guard let currentViewController = UIApplication.navigationTopViewController() else { return }
+
+        currentViewController.view.addSubview(naviView)
+        currentViewController.navigationController?.navigationBar.isHidden = true
+        naviView.snp.makeConstraints { make in
+            make.top.equalTo(currentViewController.view.safeAreaLayoutGuide)
+            make.leading.trailing.equalToSuperview()
+        }
+    }
+}
