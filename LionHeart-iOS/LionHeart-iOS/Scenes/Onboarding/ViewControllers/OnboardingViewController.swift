0d9d99e39390290e6a05266ca5bd00d191163daf
kimscastle <uiskim22@pos.idserve.net>
2023-07-11 06:11:11 +0900

    :lipstick: Onboarding NavigationBar추가

--- LionHeart-iOS/LionHeart-iOS/Scenes/Onboarding/ViewControllers/OnboardingViewController.swift
+++ LionHeart-iOS/LionHeart-iOS/Scenes/Onboarding/ViewControllers/OnboardingViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 import SnapKit
 
 final class OnboardingViewController: UIViewController {
-    
 
     public override func viewDidLoad() {
         super.viewDidLoad()
         // MARK: - 컴포넌트 설정
         setUI()
+        
+        // MARK: - 네비게이션바 설정
+        setNavigationBar()
         
         // MARK: - addsubView
         setHierarchy()
@@ -29,13 +31,20 @@ final class OnboardingViewController: UIViewController {
         
         // MARK: - delegate설정
         setDelegate()
-
     }
 }
 
 private extension OnboardingViewController {
     func setUI() {
         
+    }
+    
+    func setNavigationBar() {
+        let onboardingNavigationbar = LHNavigationBarView(type: .onboarding, viewController: self)
+            .rightFirstBarItemAction {
+                self.navigationController?.popViewController(animated: true)
+            }
+        NavigationBarLayoutManager.add(onboardingNavigationbar)
     }
     
     func setHierarchy() {
