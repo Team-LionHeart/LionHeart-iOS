fa63cfc6cc7fb68e2ebe9bf703667c84272dd351
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-07-11 11:30:45 +0900

    🤡더미데이터 만들기(#19)

---
+++ LionHeart-iOS/LionHeart-iOS/Scenes/Curriculum/Model/CurriculumDummyData.swift
@@ -0,0 +1,44 @@
+//
+//  CurriculumDummyData.swift
+//  LionHeart-iOS
+//
+//  Created by 곽성준 on 2023/07/11.
+//
+
+import UIKit
+
+
+struct CurriculumMonthData: AppData{
+    let month: String
+    let weekDatas: [CurriculumDummyData]
+}
+
+struct CurriculumDummyData: AppData {
+    let curriculumWeek: String
+    let curriculumWeekTitle: String
+    let curriculumImage: UIImageView
+    let curriculumText: String
+}
+extension CurriculumMonthData {
+    static func dummy() -> [CurriculumMonthData] {
+        return [CurriculumMonthData(month: "1개월", weekDatas: [
+            .init(curriculumWeek: "1주차", curriculumWeekTitle: "1주차 제목", curriculumImage: UIImageView(), curriculumText: "1주차 내용"),
+            .init(curriculumWeek: "2개월", curriculumWeekTitle: "2주차 제목", curriculumImage: UIImageView(), curriculumText: "2주차 내용")
+        ]),
+                .init(month: "2개월", weekDatas: [
+                    .init(curriculumWeek: "3주차", curriculumWeekTitle: "3주차 제목", curriculumImage: UIImageView(), curriculumText: "3주차 내용"),
+                    .init(curriculumWeek: "4주차", curriculumWeekTitle: "4주차 제목", curriculumImage: UIImageView(), curriculumText: "4주차 내용")
+                ]),
+                .init(month: "3개월", weekDatas: [
+                    .init(curriculumWeek: "5주차", curriculumWeekTitle: "5주차 제목", curriculumImage: UIImageView(), curriculumText: "5주차 내용"),
+                    .init(curriculumWeek: "6주차", curriculumWeekTitle: "6주차 제목", curriculumImage: UIImageView(), curriculumText: "6주차 내용")
+                ]),
+                .init(month: "4개월", weekDatas: [
+                    .init(curriculumWeek: "7주차", curriculumWeekTitle: "7주차 제목", curriculumImage: UIImageView(), curriculumText: "7주차 내용"),
+                    .init(curriculumWeek: "8주차", curriculumWeekTitle: "8주차 제목", curriculumImage: UIImageView(), curriculumText: "8주차 내용")
+                ])
+        ]
+    }
+}
+
+
