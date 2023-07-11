378f139c8e93d36eb4205527ce25213dd824109a
kimscastle <uiskim22@pos.idserve.net>
2023-07-11 09:19:13 +0900

    :ambulance: Color asset naming수정

--- LionHeart-iOS/LionHeart-iOS/Global/Utils/Palette.swift
+++ LionHeart-iOS/LionHeart-iOS/Global/Utils/Palette.swift
@@ -8,19 +8,19 @@
 import Foundation
 
 enum Palette: String {
-    case black
-    case white
-    case gray100
-    case gray200
-    case gray300
-    case gray400
-    case gray500
-    case gray600
-    case gray700
-    case gray800
-    case gray900
-    case gray1000
-    case lionRed
-    case componentLionRed
-    case background
+    case black = "Black"
+    case white = "White"
+    case gray100 = "Gray100"
+    case gray200 = "Gray200"
+    case gray300 = "Gray300"
+    case gray400 = "Gray400"
+    case gray500 = "Gray500"
+    case gray600 = "Gray600"
+    case gray700 = "Gray700"
+    case gray800 = "Gray800"
+    case gray900 = "Gray900"
+    case gray1000 = "Gray1000"
+    case lionRed = "LionRed"
+    case componentLionRed = "ComponentLionRed"
+    case background = "Background"
 }
