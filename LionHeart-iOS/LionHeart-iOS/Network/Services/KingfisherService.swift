510711243a8e1e51fa83d70f1fe0abd5133e26bc
Kim Minjae <ffalswo2@naver.com>
2023-07-11 20:14:16 +0900

    :sparkles: KingFisherService async-await wrapping

---
+++ LionHeart-iOS/LionHeart-iOS/Network/Services/KingfisherService.swift
@@ -0,0 +1,39 @@
+//
+//  KingfisherService.swift
+//  LionHeart-iOS
+//
+//  Created by 김민재 on 2023/07/11.
+//
+
+import UIKit
+
+import Kingfisher
+
+
+final class LHKingFisherService {
+
+    static func fetchImage11(with urlString: String) async throws -> UIImage? {
+        typealias imageContinuation = CheckedContinuation<UIImage?, Error>
+
+        return try await withCheckedThrowingContinuation { imageContinuation in
+
+            guard let url = URL(string: urlString) else {
+                return imageContinuation.resume(throwing: NetworkError.urlEncodingError)
+
+            }
+            
+            let resource = KF.ImageResource(downloadURL: url, cacheKey: urlString)
+            KingfisherManager.shared.retrieveImage(with: resource) { result in
+                switch result {
+                case .success(let imageResult):
+                    let image = imageResult.image
+                    return imageContinuation.resume(returning: image)
+                case .failure(_):
+                    return imageContinuation.resume(throwing: NetworkError.fetchImageError)
+                }
+            }
+        }
+        
+    }
+
+}
