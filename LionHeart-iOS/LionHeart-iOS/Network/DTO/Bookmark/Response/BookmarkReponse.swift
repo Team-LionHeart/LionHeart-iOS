377af45c1b4309718604d8bfde02d3325044cb51
cchanmi <gkfn980@hs.ac.kr>
2023-07-17 23:38:52 +0900

    [FEAT] 북마크 DTD Reponse 생성 (#57)

---
+++ LionHeart-iOS/LionHeart-iOS/Network/DTO/Bookmark/Response/BookmarkReponse.swift
@@ -0,0 +1,20 @@
+//
+//  Bookmark.swift
+//  LionHeart-iOS
+//
+//  Created by 황찬미 on 2023/07/17.
+//
+
+import Foundation
+
+struct BookmarkResponse: DTO, Response {
+    let babyNickname: String
+    let articleSummaries: [ArticleSummaryDTO]
+}
+
+struct ArticleSummaryDTO: DTO, Response {
+    let title: String
+    let mainImageUrl: String
+    let isMarked: Bool
+    let tags: [String]
+}
