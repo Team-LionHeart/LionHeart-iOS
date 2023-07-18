faa882939eb815407d80c4a1942e1bad51aeb5c1
kimscastle <uiskim22@pos.idserve.net>
2023-07-18 11:28:15 +0900

    [FEAT] ArticleService 오늘의 아티클 조회 API 구현  #59

--- LionHeart-iOS/LionHeart-iOS/Network/Services/ArticleService.swift
+++ LionHeart-iOS/LionHeart-iOS/Network/Services/ArticleService.swift
@@ -5,4 +5,22 @@
 //  Created by uiskim on 2023/07/18.
 //
 
-import Foundation
+import UIKit
+
+final class ArticleService: Serviceable {
+    static let shared = ArticleService()
+    private init() {}
+    
+    func inquiryTodayArticle() async throws -> TodayArticle {
+        let urlRequest = try NetworkRequest(path: "/v1/article/today", httpMethod: .get).makeURLRequest(isLogined: true)
+        let (data, _) = try await URLSession.shared.data(for: urlRequest)
+        
+        let model = try handleErrorCode(data: data, decodeType: TodayArticleResponse.self)
+        
+        guard let model else {
+            throw NetworkError.jsonDecodingError
+        }
+        
+        return .init(fetalNickname: model.babyNickname, articleTitle: model.title, articleDescription: model.editorNoteContent, currentWeek: model.week, currentDay: model.day, mainImageURL: model.mainImageUrl)
+    }
+}
