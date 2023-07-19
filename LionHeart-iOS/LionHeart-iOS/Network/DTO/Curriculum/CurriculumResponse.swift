7287b2de7fc9fee718de25f80c1138a36d190074
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2023-07-19 20:16:05 +0900

    [FEAT] 주차별 리스트뷰 네트워크 Service, DTOResponse 구현(#86)

--- LionHeart-iOS/LionHeart-iOS/Network/DTO/Curriculum/CurriculumResponse.swift
+++ LionHeart-iOS/LionHeart-iOS/Network/DTO/Curriculum/CurriculumResponse.swift
@@ -5,7 +5,7 @@
 //  Created by 곽성준 on 2023/07/19.
 //
 
-import Foundation
+import UIKit
 
 struct CurriculumResponse: DTO, Response {
     
@@ -13,5 +13,24 @@ struct CurriculumResponse: DTO, Response {
     let day: Int
     let progress: Int
     let remainingDay: Int
-
+    
 }
+
+struct CurriculumListByWeekResponse: DTO, Response {
+    
+    let categoryArticles: [CategoryArticle]
+    
+}
+
+struct CategoryArticle: Response {
+    
+    let articleId: Int
+    let title: String
+    let mainImageUrl: String
+    let firstBodyContent: String
+    let requiredTime: Int
+    let isMarked: Bool
+    let tags: [String]
+    
+}
+
--- LionHeart-iOS/LionHeart-iOS/Network/Services/CurriculumViewService.swift
+++ LionHeart-iOS/LionHeart-iOS/Network/Services/CurriculumViewService.swift
@@ -8,10 +8,13 @@
 import UIKit
 
 final class CurriculumService: Serviceable {
+    
     static let shared = CurriculumService()
+    
     private init() {}
     
     func getCurriculumServiceInfo() async throws -> UserInfoData {
+        
         let urlRequest = try NetworkRequest(path: "/v1/curriculum/progress", httpMethod: .get).makeURLRequest(isLogined: true)
         
         let (data, _) = try await URLSession.shared.data(for: urlRequest)
@@ -19,5 +22,22 @@ final class CurriculumService: Serviceable {
         guard let model = try dataDecodeAndhandleErrorCode(data: data, decodeType: CurriculumResponse.self) else { return UserInfoData.emptyUserInfoData }
         
         return UserInfoData(userWeekInfo: model.week, userDayInfo: model.day, progress: model.progress + 1, remainingDay: model.remainingDay)
+    }
+    
+    
+    func getArticleListByWeekInfo(week: Int) async throws -> [ArticleDataByWeek] {
+        
+        let urlRequest = try NetworkRequest(path: "v1/article/week/\(week)", httpMethod: .get).makeURLRequest(isLogined: true)
+        
+        let (data, _) = try await URLSession.shared.data(for: urlRequest)
+        
+        guard let model = try dataDecodeAndhandleErrorCode(data: data, decodeType: CurriculumListByWeekResponse.self) else {
+            return [ArticleDataByWeek.emptyData]
+        }
+        
+        return model.categoryArticles.map { article in
+            ArticleDataByWeek(articleId: article.articleId, articleTitle: article.title, articleImage: article.mainImageUrl, articleContent: article.firstBodyContent, articleReadTime: article.requiredTime, isArticleBookmarked: article.isMarked, articleTags: article.tags)
+        }
+        
     }
 }
