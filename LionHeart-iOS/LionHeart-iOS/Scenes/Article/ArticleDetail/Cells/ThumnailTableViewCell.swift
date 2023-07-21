6eec94426a73bd62516afd9591c4c9cebf588b52
Kim Minjae <ffalswo2@naver.com>
2023-07-21 21:05:47 +0900

    [FIX] imageView cell reuse되기 전 초기화 (#113)

--- LionHeart-iOS/LionHeart-iOS/Scenes/Article/ArticleDetail/Cells/ThumnailTableViewCell.swift
+++ LionHeart-iOS/LionHeart-iOS/Scenes/Article/ArticleDetail/Cells/ThumnailTableViewCell.swift
@@ -67,6 +67,10 @@ final class ThumnailTableViewCell: UITableViewCell, TableViewCellRegisterDequeue
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    override func prepareForReuse() {
+        self.thumbnailImageView.image = nil
+    }
 }
 
 private extension ThumnailTableViewCell {
