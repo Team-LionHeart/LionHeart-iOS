97a7edb58d2602a3236c4c7cccf85bffde239c36
cchanmi <gkfn980@hs.ac.kr>
2023-07-21 18:23:51 +0900

    [FIX] 커리큘럼 indexPath error 수정 (#112)

--- LionHeart-iOS/LionHeart-iOS/Scenes/Curriculum/ViewControllers/CurriculumListByWeekViewController.swift
+++ LionHeart-iOS/LionHeart-iOS/Scenes/Curriculum/ViewControllers/CurriculumListByWeekViewController.swift
@@ -154,9 +154,10 @@ private extension CurriculumListByWeekViewController {
                 guard let indexPath = notification.userInfo?["bookmarkCellIndexPath"] as? Int else { return }
                 guard let buttonSelected = notification.userInfo?["bookmarkButtonSelected"] as? Bool else { return }
                 guard let listByWeekDatas else { return }
-                try await BookmarkService.shared.postBookmark(BookmarkRequest(articleId: listByWeekDatas.articleData[indexPath].articleId,
+                try await BookmarkService.shared.postBookmark(BookmarkRequest(articleId: listByWeekDatas.articleData[indexPath-1].articleId,
                                                                               bookmarkStatus: buttonSelected))
                 hideLoading()
+                buttonSelected ? LHToast.show(message: "북마크가 추가되었습니다") : LHToast.show(message: "북마크가 해제되었습니다")
             } catch {
                 guard let error = error as? NetworkError else { return }
                 handleError(error)
