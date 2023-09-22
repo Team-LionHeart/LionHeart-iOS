74d9c4a2cae916742aee78524686d1905f69680f
cchanmi <113524@naver.com>
2023-09-23 00:49:26 +0900

    주석 삭제

--- LionHeart-iOS/LionHeart-iOS/Network/Services/BookmarkService.swift
+++ LionHeart-iOS/LionHeart-iOS/Network/Services/BookmarkService.swift
@@ -35,35 +35,4 @@ final class BookmarkService: BookmarkInOutServiceProtocol, BookmarkOutProtocol {
         guard let data = try await bookmarkAPIProtocol.getBookmark() else { return BookmarkAppData(nickName: "", articleSummaries: [])}
         return data.toAppData()
     }
-    
-    /*
-     기존의 코드 : 네트워크 요청, URLSession 통신, decode, appData return을 하나의 메서드에서 실행함
-     */
-    
-//    func getBookmark() async throws -> BookmarkAppData {
-//        let urlRequest = try NetworkRequest(path: "/v1/article/bookmarks", httpMethod: .get)
-//            .makeURLRequest(isLogined: true)
-//
-//        let (data, _) = try await URLSession.shared.data(for: urlRequest)
-//
-//        let model = try dataDecodeAndhandleErrorCode(data: data, decodeType: BookmarkResponse.self)
-//
-//        return BookmarkAppData(nickName: model?.babyNickname ?? "",
-//                               articleSummaries: model?.articleSummaries.map {
-//            ArticleSummaries(title: $0.title, articleID: $0.articleId, articleImage: $0.mainImageUrl,
-//                             bookmarked: $0.isMarked, tags: $0.tags)} ?? [])
-//    }
-    
-    
-//    func postBookmark(_ model: BookmarkRequest) async throws {
-//        let param = model.toDictionary()
-//        let body = try JSONSerialization.data(withJSONObject: param)
-//
-//        let urlRequest = try NetworkRequest(path: "/v1/article/bookmark", httpMethod: .post, body: body).makeURLRequest(isLogined: true)
-//
-//        let (data, _) = try await URLSession.shared.data(for: urlRequest)
-//        try dataDecodeAndhandleErrorCode(data: data, decodeType: String.self)
-//
-//        return
-//    }
 }
