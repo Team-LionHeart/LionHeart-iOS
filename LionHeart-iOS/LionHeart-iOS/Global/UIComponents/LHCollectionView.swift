931d936ff25368174878544105d5cc684885919b
kimscastle <uiskim22@pos.idserve.net>
2023-09-24 16:17:49 +0900

    💄 LHCollectionView 추가

---
+++ LionHeart-iOS/LionHeart-iOS/Global/UIComponents/LHCollectionView.swift
@@ -0,0 +1,23 @@
+//
+//  LHCollectionView.swift
+//  LionHeart-iOS
+//
+//  Created by uiskim on 2023/09/24.
+//
+
+import UIKit
+
+final class LHCollection: UICollectionView {
+    
+    init(color: UIColor? = .designSystem(.background), scroll: Bool = true) {
+        super.init(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        self.backgroundColor = color
+        self.showsVerticalScrollIndicator = false
+        self.showsHorizontalScrollIndicator = false
+        self.isScrollEnabled = scroll
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
