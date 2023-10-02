eb91c929c87c25ce58d0045e711e261d108e4bb6
Kim Minjae <ffalswo2@naver.com>
2023-10-02 22:27:44 +0900

    [ADD] Button 메서드 추가 (#136)
    - chaining method

--- LionHeart-iOS/LionHeart-iOS/Global/UIComponents/LHImageButton.swift
+++ LionHeart-iOS/LionHeart-iOS/Global/UIComponents/LHImageButton.swift
@@ -16,4 +16,31 @@ final class LHImageButton: UIButton {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+
+    @discardableResult
+    func setTitle(font: Font.PretendardType, text: String, color: Palette) -> Self {
+        self.titleLabel?.font = .pretendard(.subHead2)
+        self.setTitle(text, for: .normal)
+        self.setTitleColor(.designSystem(color), for: .normal)
+        return self
+    }
+
+    @discardableResult
+    func setCornerRadius(for radius: CGFloat) -> Self {
+        self.layer.cornerRadius = radius
+        self.clipsToBounds = true
+        return self
+    }
+    
+    @discardableResult
+    func setMarginImageWithText(for margin: CGFloat) -> Self {
+        self.marginImageWithText(margin: margin)
+        return self
+    }
+
+    @discardableResult
+    func setBackgroundColor(color: Palette) -> Self {
+        self.backgroundColor = .designSystem(color)
+        return self
+    }
 }
