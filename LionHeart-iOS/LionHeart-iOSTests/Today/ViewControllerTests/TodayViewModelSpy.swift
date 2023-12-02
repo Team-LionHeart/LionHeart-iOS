b5312088deb9007bfec94c1c3f9543b8b065524d
MINJAE KIM <ffalswo2@naver.com>
2023-12-03 03:07:39 +0900

    [TEST] TodayViewModelSpy 생성 (#197)

--- LionHeart-iOS/LionHeart-iOSTests/Today/ViewControllerTests/TodayViewModelSpy.swift
+++ LionHeart-iOS/LionHeart-iOSTests/Today/ViewControllerTests/TodayViewModelSpy.swift
@@ -10,6 +10,46 @@ import Combine
 
 @testable import LionHeart_iOS
 
-final class TodayViewModelSpy {
+final class TodayViewModelSpy: TodayViewModel {
     
+    var isViewWillAppeared = PassthroughSubject<Bool, Never>()
+    var isArticleTapped = PassthroughSubject<Bool, Never>()
+    var isNaviLeftButtonTapped = PassthroughSubject<Bool, Never>()
+    var isNaviRightButtonTapped = PassthroughSubject<Bool, Never>()
+    
+    var todayArticle: TodayArticle = TodayArticle.emptyArticle
+    
+    private var cancelBag = Set<AnyCancellable>()
+    
+    func transform(input: LionHeart_iOS.TodayViewModelInput) -> LionHeart_iOS.TodayViewModelOutput {
+        
+        input.viewWillAppearSubject
+            .sink { [weak self] _ in
+                self?.isViewWillAppeared.send(true)
+            }
+            .store(in: &cancelBag)
+        
+        input.navigationLeftButtonTapped
+            .sink { [weak self] _ in
+                self?.isNaviLeftButtonTapped.send(true)
+            }
+            .store(in: &cancelBag)
+        
+        input.navigationRightButtonTapped
+            .sink { [weak self] _ in
+                self?.isNaviRightButtonTapped.send(true)
+            }
+            .store(in: &cancelBag)
+        
+        input.todayArticleTapped
+            .sink { [weak self] _ in
+                self?.isArticleTapped.send(true)
+            }
+            .store(in: &cancelBag)
+        
+        
+        let output = Just(todayArticle).eraseToAnyPublisher()
+        
+        return Output(viewWillAppearSubject: output)
+    }
 }
